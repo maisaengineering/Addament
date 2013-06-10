bfcf61d0d369fd5dc2cc1a99038a51a3ae007408
maisa engineering <contact@maisasolutions.com>
2013-06-10 11:46:26 +0530

    changed goals and milestones

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,11 @@
 class Todo < ActiveRecord::Base
    attr_accessible :title, :description, :start_time, :end_time, :event_date, :user_id
   belongs_to :user
+   validate :start_must_be_before_end_time
+
+
+   def start_must_be_before_end_time
+     errors.add(:end_time, " end time must be greater than start time")unless
+         self.start_time < self.end_time
+   end
 end
