86639b0c716fc46b5cc4f9596685edb2ca50009e
maisaengineering <contact@maisasolutions.com>
2013-06-17 18:06:35 +0530

    fixed the conflict

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,4 +1,4 @@
 class Education < ActiveRecord::Base
-  attr_accessible :activities_and_societies, :date_attended, :degree, :description, :field_of_study, :grade, :school_id, :skills, :profile_id
+  attr_accessible :activities_and_societies, :date_attended, :degree, :description, :field_of_study, :grade, :school, :school_id, :skills, :profile_id
   belongs_to :profile
 end
