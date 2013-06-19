a7ba7b14ea34a81e4268ea7cc4ef3353df51189b
maisa engineering <contact@maisasolutions.com>
2013-06-19 18:03:11 +0530

    changed conversation

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -9,4 +9,10 @@ class FeedbackController < ApplicationController
     #NotificationMailer.customer_email(params[:email]).deliver
     redirect_to my_account_path
   end
+
+  def change_priority
+       update_priority = Notification.where("conversation_id = ?", params[:conversation_id]).first
+       update_priority.update_column(:priority, params[:priority])
+    #raise update_priority.inspect
+  end
 end
