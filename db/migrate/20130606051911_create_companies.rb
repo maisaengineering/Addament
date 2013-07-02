645abca87030f0a770342deab7bb98b899e53cbc
maisa engineering <contact@maisasolutions.com>
2013-07-02 13:05:25 +0530

    changed datepicker

--- db/migrate/20130606051911_create_companies.rb
+++ db/migrate/20130606051911_create_companies.rb
@@ -2,6 +2,7 @@ class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
       t.string :company_name
+      t.string :admin_email
 
       t.timestamps
     end
