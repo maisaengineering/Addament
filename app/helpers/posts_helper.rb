cfa2f5ecee3b603f01c641a4cf9f5f3929b16871
maisaengineering <contact@maisasolutions.com>
2013-07-17 15:31:10 +0530

    changed minor styles

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,20 @@
 module PostsHelper
   def comments_count(post_id)
-     @comment_count = Comment.where(post_id: post_id).count
+   comment_count = post_id.comments.count
+  end
+
+  def check_users_count
+    user = User.where("id != ?", current_user.id)
+    i = 0
+    user.each do |singleuser|
+     unless current_user.following?(singleuser)
+       reqtomentor = Requesttomentor.where(user_id: current_user.id, following_id:singleuser.id ).first
+        unless reqtomentor
+          i += 1
+       end
+       end
+    end
+    return i
 
   end
 end
